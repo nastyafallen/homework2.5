3e4f95b7db732d945921c23293a133324629e480
nastyafallen <nastyafallen@rambler.ru>
2022-06-29 00:00:39 +0300

    Коммит

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        private String login;
+        private String password;
+        private String confirmPassword;
+
+        public static void doSmth(String login, String password, String confirmPassword) {
+
+        }
+    }
+}
